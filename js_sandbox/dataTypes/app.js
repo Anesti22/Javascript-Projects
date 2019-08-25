98b94d657626b0a73163de7e53712fa07f00bee9
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-08-25 19:23:47 +0200

    Datatypes

---
+++ js_sandbox/dataTypes/app.js
@@ -0,0 +1,43 @@
+// PRIMITIVE TYPES
+
+// String
+const name = 'Jane Doe';
+
+// Boolean
+const Lali = true;
+
+// Null
+const thing = null // Is an primitive type, it will dispaly object but is a primitive type
+
+// undefine
+let red;
+
+// Number
+const age = 46;
+
+// Symbol
+const sym = Symbol()
+
+// console.log(typeof sym);
+
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'football', 'games', 'code', 'Reading the Bible'];
+
+// Object literat
+const hobbiesObj = {
+    movies: 'Game of Thrones',
+    football: 'ball',
+    games: 'Need for speed',
+    code: 'JavaSript',
+    Reading: {
+        bible: 'Book Of John',
+        hygo: 'Njeriu qe qesh'
+    }
+};
+
+const today = new Date();
+console.log(today);
+
+console.log(typeof today);
